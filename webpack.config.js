2c71ce6728b41102dcccd8eb043036e445da58b5
Lincon Kusunoki <linconkusunoki@gmail.com>
2021-12-26 22:11:13 +0100

    configure webpack with vanilla js

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    open: true,
+  },
+}
